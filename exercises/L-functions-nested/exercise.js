7f7c48ff0ce443af515c2c511d9fa52c548f0c2d
Abdulmajidram <115878433+Abdulmajidram@users.noreply.github.com>
2022-11-22 13:47:32 +0000

    L-functions-nested exercise
    used method for uppercase.
    also managed to get the solution in slack was bit difficult

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,17 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function nameUpperCase(name) {
+    return name.toUpperCase();
+}
+
+function Greeting(name) {
+    return `HELLO ${nameUpperCase(name)}`;
+}
+
+console.log(Greeting(mentor1));
+console.log(Greeting(mentor2));
+console.log(Greeting(mentor3));
+console.log(Greeting(mentor4));
+console.log(Greeting(mentor5));
